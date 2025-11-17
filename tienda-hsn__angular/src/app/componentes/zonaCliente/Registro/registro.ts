81828d70e2de7e7311cc64c3fd1c1cc46f973c3e
Alberto Martinez <albermh16@gmail.com>
2025-11-17 20:46:32 +0100

    Creando y validando formulario

--- tienda-hsn__angular/src/app/componentes/zonaCliente/Registro/registro.ts
+++ tienda-hsn__angular/src/app/componentes/zonaCliente/Registro/registro.ts
@@ -1,18 +1,33 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [ ReactiveFormsModule, JsonPipe],
   templateUrl: './registro.html',
   styleUrl: './registro.css',
 })
 export class Registro {
-  itemsPanelRegistro: string[] = [
-              "Accederás a promociones y descuentos antes que nadie.",
-              "Acumularás puntos = dinero para futuras compras.",
-              "Recibirás cupones, regalos sorpresa sólo para registrados.",
-              "Podrás invitar a tus amigos y conseguir 5€ en futuras compras.",
-              "Puedes cargar tus pedidos anteriores con un solo click.",
-              "Y mucho más...",
-  ];
-}
+  miform: FormGroup = new FormGroup(
+    {
+      nombre: new FormControl('',[
+                                    Validators.required,
+                                    Validators.minLength(3),
+                                    Validators.maxLength(20)
+                                  ]
+                                ), //<--- objeto FormControl mapea a <input name='nombre' usando directiva FormControlName: formControlName="nombre_FormControl"
+      apellidos: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl(),
+      planAmigo: new FormControl(),
+      genero: new FormControl(),
+    }
+  ); //<---objeto a mapear con tag <form...> usando directiva FormGroupDirective: [formGroup]="miform" (ngSubmit)="onSubmit()"
+
+
+  MandarDatosRegistro():void{
+    console.log('Datos del formulario de registro a mandar a nodejs....', this.miform.value);
+
+  }
+} 
